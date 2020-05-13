c63d84415c2f65217225a1c2363b9d68d5b43cb0
Joel E Carlson <joel.elmer.carlson@gmail.com>
2020-05-13 14:55:34 -0500

    1589399734

--- event.py
+++ event.py
@@ -2,6 +2,7 @@
 import asyncio
 import os
 import time
+import sys
 import yaml
 
 def checkpoint(status):
@@ -22,6 +23,19 @@ def checkpoint(status):
         when = status['end']
     elapsed = "%02.2f" %(status['duration'])
     print(f'[ {verb} ] {name} at {when}... elapsed={elapsed}, result={result}')
+
+def factorial(filename, number=0):
+    """factorial
+
+    :param filename: (str)
+    :param number: (int)
+    :returns: (int)"""
+    touch(filename)
+    fact = 1
+    for i in range(2, number + 1):
+        fact *= i
+        time.sleep(1)
+    return fact
 
 def green(message):
     """:returns: (str)"""
@@ -52,23 +66,23 @@ def loader(filename):
     return contents
 
 async def main():
-    """async main"""
+    """async main builds sandbox and calls functions"""
     filename = sandbox()
 
     await asyncio.gather(
-        stage('init', 1, filename, None, sandbox_check),
-        stage('input', 2, filename, {'parent': 'init'}),
-        stage('serverdata', 2, filename, {'parent': 'input'}),
-        stage('powercheck', 2, filename, {'parent': 'serverdata'}),
-        stage('applybios', 5, filename, {'parent': 'powercheck'}),
-        stage('raidsetup', 9, filename, {'parent': 'powercheck'}),
-        stage('poweroff', 2, filename, {'parent': 'raidsetup'}),
-        stage('isocreate', 2, filename, {'parent': 'serverdata'}),
-        stage('deploy', 9, filename, {'parent': 'poweroff'}),
-        stage('ssh', 3, filename, {'parent': 'deploy'}),
-        stage('secureboot', 2, filename, {'parent': 'ssh'}),
-        stage('email', 2, filename, {'parent': 'secureboot'}),
-        stage('alldone', 1, filename, {'parent': 'email'}),
+        stage('init', filename, sandbox_check),
+        stage('input', filename, factorial, kwargs={'parent': 'init', 'number': 2}),
+        stage('serverdata', filename, factorial, kwargs={'parent': 'input', 'number': 2}),
+        stage('powercheck', filename, factorial, kwargs={'parent': 'serverdata', 'number': 3}),
+        stage('applybios', filename, factorial, kwargs={'parent': 'powercheck', 'number': 5}),
+        stage('raidsetup', filename, factorial, kwargs={'parent': 'powercheck', 'number': 9}),
+        stage('poweroff', filename, factorial, kwargs={'parent': 'raidsetup', 'number': 3}),
+        stage('isocreate', filename, factorial, kwargs={'parent': 'serverdata', 'number': 5}),
+        stage('deploy', filename, factorial, kwargs={'parent': 'poweroff', 'number': 9}),
+        stage('ssh', filename, factorial, kwargs={'parent': 'deploy', 'number': 2}),
+        stage('secureboot', filename, factorial, kwargs={'parent': 'ssh', 'number': 2}),
+        stage('email', filename, factorial, kwargs={'parent': 'secureboot', 'number': 2}),
+        stage('alldone', filename, factorial, kwargs={'parent': 'email', 'number': 2}),
     )
 
 def red(message):
@@ -82,80 +96,84 @@ def sandbox(path='/var/tmp/', filename='stage.yaml'):
     :param filename: (str)
     :returns journalfile: (str)"""
     journalfile = path + filename
+    try:
+        os.remove(journalfile)
+    except FileNotFoundError:
+        pass
     os.chdir(path)
     touch(journalfile)
     return journalfile
 
-def sandbox_check(filename, **kwargs):
+def sandbox_check(filename, number=0):
     """sandbox_check check the sandbox
 
     :param filename: (str)
-    :param **kwargs: (dict)"""
+    :returns: (bool)"""
     print('*** sandbox_check ***')
-    for key, value in kwargs.items():
-        print(key, value)
     os.system('df -h ' + filename)
     print('*********************')
+    time.sleep(number)
+    return True
 
-async def stage(name, number, filename, dependent=None, *args, **kwargs):
-    """stage executes functions in *args, **kwargs
+async def stage(name, filename, *args, **kwargs):
+    """stage executes *args w/ **kwargs
 
-    :param name:      (str)  name of function
-    :param number:    (int)  demo arg for factorial
+    :param name:      (str)  unique name of our step
     :param filename:  (yaml) journal our events
-    :param dependent: (dict) child is dependent on parent
-    :param *args:     [str]
+    :param *args:     [str]  function to process
     :param **kwargs:  (dict)"""
-    # handle dependencies and status
+
+    # Process *args, **kwargs
+    number = 0
+    parent = None
+    process = None
+    if len(args) > 0:
+        process = args[0]
+    else:
+        print(f'async def stage({name}, {filename}, {args}, {kwargs}) error')
+        sys.exit(1)
+    for key, value in kwargs.items():
+        if key == 'kwargs':
+            number = value['number']
+            parent = value['parent']
+
+    # Manage dependencies and start time
     status = loader(filename)
-    if dependent is not None and status is not None:
+    if parent is not None and status is not None:
         ready = 0
         while ready < 1:
             for key, value in status.items():
-                if key == dependent['parent'] and value['end'] > 0:
+                if key == parent and value['end'] > 0:
                     ready = 1
             await asyncio.sleep(5)
             status = loader(filename)
 
     # status=0: Start
     run = {'name': name,
-           'number': number,
            'start': time.time(),
            'end': 0,
            'duration': 0,
            'status': 0,
            'result': 0,
-           'dependent': dependent}
+           'kwargs': kwargs}
     journal(filename, run)
 
-    # status=1: Run (factorials for fun)
-    for k in args:
-        k(filename, **kwargs)
-        await asyncio.sleep(1)
-
-    # factorials simulate something that takes time
-    # replace in production code w/ IO functions
-    factorial = 1
-    for i in range(2, number + 1):
-        factorial *= i
-        await asyncio.sleep(1)
+    # status=1: Run
+    result = process(filename, number)
+    await asyncio.sleep(1)
 
     run['status'] = 1
     run['duration'] = time.time() - run['start']
+    run['result'] = result
     journal(filename, run)
 
     # status=2: Done
-    run['end'] = time.time()
     run['status'] = 2
-    run['result'] = factorial
+    run['end'] = time.time()
     journal(filename, run)
 
 def touch(filename, times=None):
     """touch filename"""
-    try:
-        os.remove(filename)
-    except FileNotFoundError:
-        pass
     with open(filename, 'a'):
         os.utime(filename, times)
 
